03f02c1b41d57aa1f79869d0a48237e41929f967
jhnnsrs <jhnnsrs@gmail.com>
2021-11-03 16:45:26 +0100

    no longer optional

--- koil/qt.py
+++ koil/qt.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 Reference = str
 
+
 class UnconnectedSignalError(Exception):
     pass
 
@@ -22,9 +23,6 @@ def get_receiver_length(qobject, qsignal, callstring):
         return qobject.receivers(callstring)
 
 
-
-
-
 class FutureWrapper(QObject):
     call = Signal(Reference, tuple, dict)
     resolve = Signal(Reference, object)
@@ -32,26 +30,23 @@ class FutureWrapper(QObject):
     cancel = Signal(Reference, tuple, dict)
     cancelled = Signal(Reference)
 
-    
-    def __init__(self, *args, koil: Koil = None, cancel_timeout = 4, pass_through=False, **kwargs) -> None:
-        super().__init__( *args, **kwargs)
+    def __init__(self, *args, cancel_timeout=4, pass_through=False, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.resolve.connect(self.on_resolve)
         self.reject.connect(self.on_reject)
         self.cancelled.connect(self.on_cancelled)
         self.futureMap = {}
         self.cancelMap = {}
-        self.koil = koil or get_current_koil()
-        self.loop = self.koil.loop
         self.pass_through = pass_through
         self.cancel_timeout = 4
 
-
     def on_cancelled(self, ref):
-        assert ref in self.cancelMap, "Received cancellation for non invoked cancel future"
+        assert (
+            ref in self.cancelMap
+        ), "Received cancellation for non invoked cancel future"
         cancel_future = self.cancelMap[ref]
         if not cancel_future.done():
             self.loop.call_soon_threadsafe(cancel_future.set_result, None)
-
 
     def on_resolve(self, ref, obj):
         assert ref in self.futureMap, "Received resolvation for non invoked future"
@@ -59,23 +54,21 @@ class FutureWrapper(QObject):
         if not future.done():
             self.loop.call_soon_threadsafe(future.set_result, obj)
 
-
     def on_reject(self, ref, exp):
         assert ref in self.futureMap, "Received rejection for non invoked future"
         future = self.futureMap[ref]
         if not future.done():
             self.loop.call_soon_threadsafe(future.set_exception, exp)
 
-
-
     async def acall(self, *args, **kwargs):
+        self.loop = asyncio.get_event_loop()
 
         if get_receiver_length(self, self.call, "call(str, tuple, dict)") == 0:
-           if self.pass_through: return None
-           else: raise UnconnectedSignalError("This future has no connected receivers")
+            if self.pass_through:
+                return None
+            else:
+                raise UnconnectedSignalError("This future has no connected receivers")
 
-
-        
         reference = str(uuid.uuid4())
         future = self.loop.create_future()
         self.futureMap[reference] = future
@@ -93,20 +86,16 @@ class FutureWrapper(QObject):
                 except asyncio.TimeoutError as e:
                     logger.error("Cancellation recevied timeout, Cancelling anyways")
                     raise e
-            else: 
+            else:
                 raise e
 
 
-
-
-
-
 class TimeoutFutureWrapper(FutureWrapper):
-
     def __init__(self, *args, timeout=None, **kwargs) -> None:
-        super().__init__( *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.timeout = timeout
 
     async def acall(self, *args, **kwargs):
-        return await asyncio.wait_for(super().acall(*args, **kwargs), timeout=self.timeout)
-
+        return await asyncio.wait_for(
+            super().acall(*args, **kwargs), timeout=self.timeout
+        )
--- koil/task.py
+++ koil/task.py
@@ -2,30 +2,27 @@ import logging
 import asyncio
 
 
-
-
 logger = logging.getLogger(__name__)
 
 
 class KoilTask:
-
-    def __init__(self, future=None, koil = None, *args, log_errors=True, **kwargs) -> None:
+    def __init__(
+        self, future=None, koil=None, *args, log_errors=True, **kwargs
+    ) -> None:
         super().__init__(*args, **kwargs)
-        from koil.koil import get_current_koil
         self.future = future
         self.task = None
         self.log_errors = log_errors
-        self.koil = koil or get_current_koil()
-        self.loop = self.koil.loop
+        self.loop = koil.loop
         self.loop.call_soon_threadsafe(self.run)
 
     async def wrapped_future(self, future):
         try:
             return await future
         except Exception as e:
-            if self.log_errors: logger.exception(e)
+            if self.log_errors:
+                logger.exception(e)
             raise e
-
 
     def run(self):
         self.task = self.loop.create_task(self.wrapped_future(self.future))
